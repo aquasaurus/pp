60503fab74d6c40fc1fdb39d44e5c1fd52d31958
Schatten et Caligo <psi@innocent.com>
2022-07-21 20:49:26 +0530

    add svgs

---
+++ components/form/Button/Success.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    class="
+      p-4
+      flex flex-row text-white
+      items-center
+      rounded-md
+      bg-gray-700
+      hover:bg-gray-600
+      transition
+      duration-500
+      ease-in-out
+      transform
+      hover:-translate-y-1
+    "
+    @click="
+      (x) => {
+        $emit('okay');
+      }
+    "
+  >
+    {{ caption }}
+  </button>
+</template>
+<script setup>
+defineProps({
+  caption: {
+    type: String,
+    default: null,
+  },
+});
+</script>
---
+++ components/form/Text/Number.vue
@@ -0,0 +1,32 @@
+<template>
+    <div :class = "`flex flex-row items-center space-x-2`">
+        <div class = "px-2 bg-white text-zinc-900 border border-gray-800 dark:border-gray-100 border-zinc-900 dark:bg-gray-700 dark:text-white dark:border-zinc-100 flex flex-row space-x-4">
+            <span class = "">{{prefix}}</span>
+            <input type = "number" :placeholder="somePlaceholder" :id = "`text_field_${friendlyName.replace(/\s/g, '_')}`" v-model="myContent" class = "w-72 h-8 p-2 bg-white focus:outline-0" @input="x => {$emit('update', myContent)}" />
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    content: {
+        type: Number,
+        default: () => null,
+
+    },
+    prefix: {
+        type: String,
+        default: () => "",
+
+    },
+    friendlyName: {
+        type: String,
+        default: () => "test"
+        
+    },
+    somePlaceholder: {
+    type: String,
+    default: () => null
+}
+})
+const myContent = ref(props.content)
+</script>
