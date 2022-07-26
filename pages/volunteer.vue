72a8e4e07ac738b6ba750de89e4eab16cba3b455
Schatten et Caligo <psi@innocent.com>
2022-07-26 23:08:34 +0530

    public dir

--- pages/volunteer.vue
+++ pages/volunteer.vue
@@ -1,8 +1,26 @@
 <template>
-    <div></div>
+  <div>
+    <div class="grid grid-cols-1 lg:grid-cols-3">
+        <div v-if = "items.error" />
+        <div v-else-if = "items.pending" />
+        <div v-else v-for="report in items.data" :key="report.n">
+            <span>{{report.n}}</span>
+            <span>{{report.p}}</span>
+            <span>{{report.c}}</span>
+            <span>{{Date(report.t)}}</span>
+        </div>
+    </div>
+  </div>
 </template>
 <script setup>
 definePageMeta({
-    layout: "dashboard",
-})
+  layout: "dashboard",
+});
+
+const items = useFetch({
+    url: "http://localhost:8000/reports/all",
+    method: "GET",
+    headers: { contentType: "application/json" },
+  });
+  console.log(items)
 </script>
