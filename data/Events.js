96b2bc32b566903b3c168e423ccf1d6076372271
MukulRay <mukulr2003@gmail.com>
2022-07-29 13:20:19 +0530

    events updated

--- data/Events.js
+++ data/Events.js
@@ -1,7 +1,7 @@
 export default [
   {
     title: {
-      en: "",
+      en: "An awareness event for women and children - Chennai Central Railway Station - presence of DIG Balakrishnan and Judge Jayanthi and police officers.",
       ta:
         "சென்னை சென்ட்ரல் ரயில்வே நிலையத்தில் பெண்களுக்கும் குழந்தைகளுக்குமான விழிப்புணர்வு நிகழ்வு டிஐஜி பாலகிருஷ்ணன் மற்றும் ஜட்ஜ் ஜெயந்தி மற்றும் காவல்துறையினர்கள் முன்னிலையில் சிறப்பாக நடைபெற்றது",
     },
@@ -17,7 +17,7 @@ export default [
   },
   {
     title: {
-      en: "International Women's Day Celebration - Chennai Central Railway Station",
+      en: "Awareness event for students about drugs - ICF High School",
       ta:
         "போதைப் பொருட்களைப் பற்றி மாணவ மாணவிகளுக்கு ஐ சி எப் மேல்நிலைப்பள்ளியில் விழிப்புணர்வு நிகழ்வு",
     },
@@ -25,7 +25,7 @@ export default [
   },
   {
     title: {
-      en: "International Women's Day Celebration - Chennai Central Railway Station",
+      en: "Public awareness event led by People's Court",
       ta:
         "மக்கள் நீதி மன்றம் நீதியரசர் ஜெயந்தி அவர்களின் தலைமையில் மக்களுக்கு விழிப்புணர்வு நிகழ்வு",
     },
@@ -33,7 +33,7 @@ export default [
   },
   {
     title: {
-      en: "International Women's Day Celebration - Chennai Central Railway Station",
+      en: "Awareness event for students and public  - Thiruvannamalai Railway Station - presence of DGP Sailendrababu",
       ta:
         "திருவண்ணாமலை ரயில் நிலையத்தில் மாணவர்களுக்கும் பொதுமக்களுக்கும் டிஜிபி சைலேந்திரபாபு அவர்களின் முன்னிலையில் விழிப்புணர்வு நிகழ்வு",
     },
@@ -41,7 +41,7 @@ export default [
   },
   {
     title: {
-      en: "Arm Yourself With GRP Help App",
+      en: "A huge public awareness event about GRP Helping & Art show - Egmore railway station",
       ta:
         "எக்மோர் ரயில் நிலையத்தில் ஜிஆர்பி ஹெல்பிங் பற்றி பொது மக்களுக்கு மிகப்பெரிய ஒரு விழிப்புணர்வு நிகழ்வு கலை நிகழ்ச்சி உடன் நடத்தப்பட்டது",
     },
@@ -49,7 +49,7 @@ export default [
   },
   {
     title: {
-      en: "Arm Yourself With GRP Help App",
+      en: "An awareness event with a short play on public safety - Chennai Central railway station",
       ta:
         "சென்னை சென்ட்ரல் ரயில் நிலையத்தில் பொதுமக்களுக்கு பாதுகாப்பு குறித்து ஒரு சிறிய நாடகத்துடன் கூடிய ஒரு விழிப்புணர்வு நிகழ்வு",
     },
@@ -57,7 +57,7 @@ export default [
   },
   {
     title: {
-      en: "Arm Yourself With GRP Help App",
+      en: "how to manage traffic congestion - Pukkadai Police Station - Students with DSP Mohan Singh Joswa and Inspector Thalavaiswamy",
       ta:
         "பூக்கடை காவல் நிலையத்தில் மாணவர்களுக்கு போக்குவரத்து நெரிசலை சீர் செய்வது எப்படி என்று டிஎஸ்பி மோகன் சிங் ஜோஸ்வா மற்றும் ஆய்வாளர் தளவாய்சாமி அவர்களுடன் மாணவர்கள்",
     },
@@ -65,7 +65,7 @@ export default [
   },
   {
     title: {
-      en: "Arm Yourself With GRP Help App",
+      en: "Appreciating the students and our community activists for smoothing the traffic - Deputy Commissioner of Police Mrs. Ramya Bharti, Senior Officer of Police",
       ta:
         "காவல்துறையின் உயர் அதிகாரியான காவல் துறை துணை ஆணையர் திருமதி ரம்யா பாரதி அவர்கள் நமது மாணவர்களுக்கும் மற்றும் நமது சமூக ஆர்வலர்களுக்கும் போக்குவரத்து நெரிசலை சீர் செய்ததற்காக பாராட்டி மாணவர்களுடன் உரையாடினார்",
     },
@@ -73,14 +73,14 @@ export default [
   },
   {
     title: {
-      en: "Arm Yourself With GRP Help App",
+      en: "The students cleared the traffic jam in the areas under the jurisdiction of the High Court - Police Inspector Mr. Jayachandran",
       ta:
         "உயர் நீதிமன்றத்தின் எல்லைக்கு உட்பட்ட இடங்களில் காவல் ஆய்வாளர் திரு ஜெயச்சந்திரன் அவர்களின் முன்னிலையில் மாணவர்கள் போக்குவரத்து நெரிசலை சீர் செய்தனர்",
     },
     images: 10,
   },
   {
-    title: {
+    title: { //Repeated with title 6
       en: "GRP Help App Inauguration - Chennai Central Railway Station",
       ta:
         "எக்மோர் ரயில் நிலையத்தில் ஜிஆர்பி ஹெல்பிங் பற்றி பொது மக்களுக்கு மிகப்பெரிய ஒரு விழிப்புணர்வு நிகழ்வு கலை நிகழ்ச்சி உடன் நடத்தப்பட்டது",
@@ -89,7 +89,7 @@ export default [
   },
   {
     title: {
-      en: "GRP Help App Inauguration - Chennai Central Railway Station",
+      en: "Awareness event for load lifting workers - Chennai Central Railway Station",
       ta:
         "சென்ட்ரல் ரயில் நிலையத்தில் சுமை தூக்கும் தொழிலாளர்களுக்கு நீதியரசர் ஜெயந்தி அவர்களும் முன்னிலையில் விழிப்புணர்வு நிகழ்வு",
     },
